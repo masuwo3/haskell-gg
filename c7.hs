232d7654d7574a02dad0076eae4ac8d6bddf671d
masuwo3 <masuwo3@gmail.com>
2018-11-28 00:09:25 +0900

    chapter 7

--- c7.hs
+++ c7.hs
@@ -1,3 +1,6 @@
+import qualified Data.Map as Map
+
+
 -- 7.2
 
 -- data Shape = Circle Float Float Float |
@@ -26,3 +29,191 @@ baseCircle r = Circle (Point 0 0) r
 
 baseRect :: Float -> Float -> Shape
 baseRect width height = Rectangle (Point 0 0) (Point width height)
+
+-- 7.3
+
+-- data Person = Person String String Int Float String String
+--     deriving (Show)
+
+-- firstName :: Person -> String
+-- firstName (Person firstName _ _ _ _ _) = firstName
+
+-- lastName :: Person -> String
+-- lastName (Person _ lastName _ _ _ _) = firstName
+
+-- age :: Person -> Int
+-- age (Person _ _ age _ _ _) = firstName
+-- ...
+
+data Person = Person { firstName :: String,
+                       lastName :: String,
+                       age :: Int,
+                       height :: Float,
+                       phoneNumber :: String,
+                       flavor :: String } deriving (Show)
+
+-- 7.4
+
+data Vector a = Vector a a a deriving (Show)
+
+vplus :: (Num a) => Vector a -> Vector a -> Vector a
+(Vector i j k) `vplus` (Vector l m n) = Vector (i+l) (j+m) (k+n)
+
+dotProd :: (Num a) => Vector a -> Vector a -> a
+(Vector i j k) `dotProd` (Vector l m n) = i*j + j*m + k*n
+
+vmult :: (Num a) => Vector a -> a -> Vector a
+(Vector i j k) `vmult` m = Vector (i*m) (j*m) (k*m)
+
+-- 7.5
+
+data Day = Monday | Tuesday | Wednessday | Thursday | Friday | Saturday | Sunday
+    deriving (Eq, Ord, Show, Read, Bounded, Enum)
+
+-- 7.6
+
+-- type String = [Char]
+
+type PhoneNumber = String
+type Name = String
+type PhoneBook = [(Name, PhoneNumber)]
+
+inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+inPhoneBook name pnumber pbook = (name, pnumber) `elem` pbook
+
+data LockerState = Taken | Free deriving (Show, Eq)
+
+type Code = String
+
+type LockerMap = Map.Map Int (LockerState, Code)
+
+lockerLookup :: Int -> LockerMap -> Either String Code
+lockerLookup lockerNumber map = case Map.lookup lockerNumber map of
+    Nothing -> Left $ "Locker " ++ show lockerNumber
+                    ++ " doesn't exist!"
+    Just (state, code) -> if state /= Taken
+                            then Right code
+                            else Left $ "Locker " ++ show lockerNumber ++ " is already taken"
+
+lockers :: LockerMap
+lockers = Map.fromList
+        [(100, (Taken, "ZD39I")),
+         (101, (Free, "JAH3I")),
+         (103, (Free, "IQSA9")),
+         (105, (Free, "QOTSA")),
+         (109, (Taken, "893JJ")),
+         (110, (Taken, "99292"))]
+
+-- 7.7
+
+-- data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
+
+data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
+
+-- infixr 5 :-:
+-- data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+
+-- infixr 5 ^++
+-- (^++) :: List a -> List a -> List a
+-- Empty ^++ ys = ys
+-- (x :-: xs) ^++ ys = x :-: (xs ^++ ys)
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+    | x == a = Node x left right
+    | x < a  = Node a (treeInsert x left) right
+    | x > a  = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+    | x == a = True
+    | x < a  = treeElem x left
+    | x > a  = treeElem x right
+
+-- 7.8
+
+-- class Eq a where
+--     (==) :: a -> a -> Bool
+--     (/=) :: a -> a -> Bool
+--     x == y = not (x /= y)
+--     x /= y = not (x == y)
+
+data TrafficLight = Red | Yellow | Green
+
+instance Eq TrafficLight where
+    Red == Red = True
+    Yellow == Yellow = True
+    Green == Green = True
+    _ == _ = False
+
+instance Show TrafficLight where
+    show Red = "Red light"
+    show Yellow = "Yellow light"
+    show Green = "Green light"
+
+-- instance (Eq m) => Eq (Maybe m) where
+--     Just x == Just y = x == y
+--     Nothing == Nothing = True
+--     _ == _ = False
+
+-- 7.9
+
+class YesNo a where
+    yesno :: a-> Bool
+
+
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+
+instance YesNo [a] where
+    yesno [] = False
+    yesno _ = True
+
+instance YesNo Bool where
+    yesno = id
+
+instance YesNo (Maybe a) where
+    yesno (Just _) = True
+    yesno Nothing = False
+
+instance YesNo (Tree a) where
+    yesno EmptyTree = False
+    yesno _ = True
+
+instance YesNo TrafficLight where
+    yesno Red = False
+    yesno _ = True
+
+yesnoIf :: (YesNo y) => y -> a -> a -> a
+yesnoIf yesnoVal yesResult noResult =
+    if yesno yesnoVal
+        then yesResult
+        else noResult
+
+-- 7.10
+
+-- instance Functor Maybe where
+--     fmap f (Just x) = Just (f x)
+--     fmap f Nothing = Nothing
+
+-- instance Functor Tree where
+--     fmap f EmptyTree = EmptyTree
+--     fmap f (Node x left right)
+--                     = Node (f x) (fmap f left) (fmap f right)
+
+-- instance Functor (Either a) where
+--     fmap f (Right x) = Right (f x)
+--     fmap f (Left x) = Left x
+
+-- data Either a b = Left a | Right b
+
+-- instance Functor (Map.Map a) where
+--     fmap f (Map.Map k v) = Map.Map k (f v)
